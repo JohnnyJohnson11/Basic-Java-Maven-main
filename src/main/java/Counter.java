07dcdcfa950a66fad1f64b5702cc911fccdc00c2
SeanArianto <92348556+JohnnyJohnson11@users.noreply.github.com>
2025-05-07 20:09:48 +0700

    Update Counter.java

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -9,15 +9,21 @@ public class Counter {
 	}
 		
 	public void reset() {
-		count = 1;
+		//Test Fail
+		//count = 1;
+		count=0;
 	}
 	
 	public void increment() {
-		count=count+2;
+		//Test Fail
+		//count=count+2;
+		count++;
 	}
 	
 	public void decrement() {
-		count=count-2;
+		//Test Fail
+		//count=count-2;
+		count--;
 	}
 	
 	public int getCount() {
